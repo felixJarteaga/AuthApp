9129644abd6a8a07dfba992870b06b7b3cd3cb15
Felix José Arteaga trigo <farteaga21791@gmail.com>
2021-12-19 17:40:08 +0100

    fin video 389

---
+++ src/app/auth/pages/register/register.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styles: [],
+})
+export class RegisterComponent {
+  miFormulario: FormGroup = this.fb.group({
+    name: ['Test 3', [Validators.required]],
+    email: ['test3@test.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
+  });
+
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  registrar() {
+    console.log(this.miFormulario.value);
+    this.router.navigateByUrl('/dashboard');
+  }
+}
